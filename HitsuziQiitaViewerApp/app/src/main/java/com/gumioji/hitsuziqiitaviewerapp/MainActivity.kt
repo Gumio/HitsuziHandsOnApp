eb4771be98665035674f76045e86c2b790cfe723
Gumio <quran.gumi3134@gmail.com>
2018-11-17 19:49:14 +0900

    first setting

--- HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainActivity.kt
+++ HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.gumioji.hitsuziqiitaviewerapp
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gumioji.hitsuziqiitaviewerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        mBinding.changeButton.setOnClickListener {
+            mBinding.helloText.text = "hello, Fuck you"
+        }
     }
 }
