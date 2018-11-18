8b777c3275dd75ad55fa6fe332b6a468195768e4
Gumio <quran.gumi3134@gmail.com>
2018-11-18 18:39:44 +0900

    add searchview

--- HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainActivity.kt
+++ HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainActivity.kt
@@ -3,12 +3,23 @@ package com.gumioji.hitsuziqiitaviewerapp
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.util.Log
+import android.view.Menu
 import com.gumioji.hitsuziqiitaviewerapp.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = this::class.java.simpleName
+    }
+
     private val mBinding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
+
+    private lateinit var mSearchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +29,33 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_container, fragment)
         transaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+
+        mSearchView = (menu?.findItem(R.id.toolbar_menu_search)?.actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(text: String?): Boolean {
+                    // 検索キーが押下された
+                    Log.d(TAG, "submit text: $text")
+                    val mainFragment = supportFragmentManager.findFragmentById(R.id.main_container)
+                    if (mainFragment is MainFragment && text != null) {
+                        mainFragment.searchRequest(text)
+                    }
+                    return false
+                }
+
+                override fun onQueryTextChange(text: String?): Boolean {
+                    // テキストが変更された
+                    Log.d(TAG, "change text: $text")
+                    return false
+                }
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
 
--- HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainFragment.kt
+++ HitsuziQiitaViewerApp/app/src/main/java/com/gumioji/hitsuziqiitaviewerapp/MainFragment.kt
@@ -1,17 +1,13 @@
 package com.gumioji.hitsuziqiitaviewerapp
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.TextUtils
 import android.util.Log
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
-import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -36,44 +32,6 @@ class MainFragment : Fragment() {
         mAdapter = ListAdapter(activity?.applicationContext!!, R.layout.list_item)
         val listView = list_view
         listView.adapter = mAdapter
-
-        val editText = edit_text
-        editText.setOnKeyListener(View.OnKeyListener { view, keyCode, keyEvent ->
-            if (keyEvent.action != KeyEvent.ACTION_UP || keyCode != KeyEvent.KEYCODE_ENTER) {
-                return@OnKeyListener false
-            }
-
-            val editText = view as EditText
-            // キーボードを閉じる
-            val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(editText.windowToken, 0)
-
-            var text = editText.text.toString()
-            try {
-                // url encode　例. スピッツ > %83X%83s%83b%83c
-                text = URLEncoder.encode(text, "UTF-8")
-            } catch (e: UnsupportedEncodingException) {
-                Log.e("", e.toString(), e)
-                return@OnKeyListener true
-            }
-
-            if (!TextUtils.isEmpty(text)) {
-                val request = QiitaClient.create().items(text)
-                Log.d("", request.request().url().toString())
-                val item = object : Callback<List<Item>> {
-                    override fun onResponse(call: Call<List<Item>>?, response: Response<List<Item>>?) {
-                        mAdapter.clear()
-                        response?.body()?.forEach { mAdapter.add(it) }
-                    }
-
-                    override fun onFailure(call: Call<List<Item>>?, t: Throwable?) {
-                    }
-                }
-                request.enqueue(item)
-            }
-            true
-        })
-
         listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
             val result = mAdapter.getItem(position)
             val manager = fragmentManager
@@ -81,6 +39,31 @@ class MainFragment : Fragment() {
             transaction.replace(R.id.main_container, DetailFragment.newInstance(result.url.toString()))
             transaction.addToBackStack(null)
             transaction.commit()
+        }
+    }
+
+    fun searchRequest(searchText: String) {
+        var text = searchText
+        try {
+            // url encode　例. スピッツ > %83X%83s%83b%83c
+            text = URLEncoder.encode(text, "UTF-8")
+        } catch (e: UnsupportedEncodingException) {
+            Log.e("", e.toString(), e)
+        }
+
+        if (!TextUtils.isEmpty(text)) {
+            val request = QiitaClient.create().items(text)
+            Log.d("", request.request().url().toString())
+            val item = object : Callback<List<Item>> {
+                override fun onResponse(call: Call<List<Item>>?, response: Response<List<Item>>?) {
+                    mAdapter.clear()
+                    response?.body()?.forEach { mAdapter.add(it) }
+                }
+
+                override fun onFailure(call: Call<List<Item>>?, t: Throwable?) {
+                }
+            }
+            request.enqueue(item)
         }
     }
 
